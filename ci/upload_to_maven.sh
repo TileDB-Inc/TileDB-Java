02f0bb21609081377da57f28385bb2ac9103f6bc
Seth Shelnutt <seth@tiledb.io>
2019-07-02 18:04:28 -0400

    Allow uploading master as snapshots to sonatype oss

---
+++ ci/upload_to_maven.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "Starting upload to maven"
+mkdir .travis
+echo "${GPG_SECRET_KEYS_ENC}" | base64 --decode > ${ENCRYPTED_GPG_KEY_LOCATION}
+openssl aes-256-cbc -K $encrypted_a2869fb015d7_key -iv $encrypted_a2869fb015d7_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
+./gradlew properties -q | grep "version:" | awk '{print $2}'
+export PROJECT_VERSION=$(./gradlew properties -q | grep "version:" | awk '{print $2}')
+# Upload only snapshots to sonatype oss so it can make its way to maven central
+./gradlew publishMavenJavaPublicationToMavenRepository
+
+# Only non-snapshot can be pushed as maven releases
+if [[ ! $(echo "${PROJECT_VERSION}" | grep "SNAPSHOT") ]]; then
+  ./gradlew closeAndReleaseRepository
+fi
+echo "Update to maven completed"
