dd2b3213523738d18d9ae7dc9b1ccadbae3682de
dimitrisstaratzis <jstaratzi@icloud.com>
2021-06-11 20:01:33 +0300

    Imported TileDB 2.3.0 bindings

--- src/main/java/io/tiledb/libtiledb/tiledb_query_condition_combination_op_t.java
+++ src/main/java/io/tiledb/libtiledb/tiledb_query_condition_combination_op_t.java
@@ -8,39 +8,41 @@
 
 package io.tiledb.libtiledb;
 
-public enum tiledb_filter_option_t {
-  TILEDB_COMPRESSION_LEVEL(0),
-  TILEDB_BIT_WIDTH_MAX_WINDOW(1),
-  TILEDB_POSITIVE_DELTA_MAX_WINDOW(2);
+public enum tiledb_query_condition_combination_op_t {
+  TILEDB_AND(0),
+  TILEDB_OR(1),
+  TILEDB_NOT(2);
 
   public final int swigValue() {
     return swigValue;
   }
 
-  public static tiledb_filter_option_t swigToEnum(int swigValue) {
-    tiledb_filter_option_t[] swigValues = tiledb_filter_option_t.class.getEnumConstants();
+  public static tiledb_query_condition_combination_op_t swigToEnum(int swigValue) {
+    tiledb_query_condition_combination_op_t[] swigValues =
+        tiledb_query_condition_combination_op_t.class.getEnumConstants();
     if (swigValue < swigValues.length
         && swigValue >= 0
         && swigValues[swigValue].swigValue == swigValue) return swigValues[swigValue];
-    for (tiledb_filter_option_t swigEnum : swigValues)
+    for (tiledb_query_condition_combination_op_t swigEnum : swigValues)
       if (swigEnum.swigValue == swigValue) return swigEnum;
     throw new IllegalArgumentException(
-        "No enum " + tiledb_filter_option_t.class + " with value " + swigValue);
+        "No enum " + tiledb_query_condition_combination_op_t.class + " with value " + swigValue);
   }
 
   @SuppressWarnings("unused")
-  private tiledb_filter_option_t() {
+  private tiledb_query_condition_combination_op_t() {
     this.swigValue = SwigNext.next++;
   }
 
   @SuppressWarnings("unused")
-  private tiledb_filter_option_t(int swigValue) {
+  private tiledb_query_condition_combination_op_t(int swigValue) {
     this.swigValue = swigValue;
     SwigNext.next = swigValue + 1;
   }
 
   @SuppressWarnings("unused")
-  private tiledb_filter_option_t(tiledb_filter_option_t swigEnum) {
+  private tiledb_query_condition_combination_op_t(
+      tiledb_query_condition_combination_op_t swigEnum) {
     this.swigValue = swigEnum.swigValue;
     SwigNext.next = this.swigValue + 1;
   }
