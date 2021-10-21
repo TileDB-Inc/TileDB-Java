05d1788d85c4c66a468501d220e43bdf9c975e22
Isaiah Norton <isaiah@tiledb.com>
2021-10-21 10:31:55 -0400

    WIP

--- ci/upload_to_maven.sh
+++ ci/upload_to_maven.sh
@@ -3,6 +3,11 @@
 export GPG_KEY_LOCATION=$(pwd)/encrypted.key
 echo "Starting upload to maven"
 echo "${GPG_SECRET_KEYS_ENC}" | base64 --decode > $GPG_KEY_LOCATION
+echo "ls key: "
+ls $GPG_KEY_LOCATION
+echo "sha256sum key: "
+sha256sum $GPG_KEY_LOCATION
+echo "running gradlew"
 ./gradlew properties -q | grep "version:" | awk '{print $2}'
 export PROJECT_VERSION=$(./gradlew properties -q | grep "version:" | awk '{print $2}')
 # Upload only snapshots to sonatype oss so it can make its way to maven central
