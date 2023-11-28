8e2b76c9fbfc99045cdefd9d1f0a0ecdafc4f732
Dimitris Staratzis <33267511+DimitrisStaratzis@users.noreply.github.com>
2023-11-28 13:36:27 +0200

    update to TileDB-2.18.1

--- src/test/java/io/tiledb/java/api/VersionTest.java
+++ src/test/java/io/tiledb/java/api/VersionTest.java
@@ -35,6 +35,6 @@ public class VersionTest {
     System.out.println(version);
     Assert.assertTrue(version.getMajor() == 2);
     Assert.assertTrue(version.getMinor() == 18);
-    Assert.assertTrue(version.getRevision() == 0);
+    Assert.assertTrue(version.getRevision() == 1);
   }
 }
